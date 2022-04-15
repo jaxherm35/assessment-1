5385d93876677e587550b573f7774bea470f0e39
Jaxon Hermansen <jaxherm35@gmail.comgit config --user.name Jaxon>
2022-04-15 12:56:47 -0600

    finalpasswordcommit

--- password.js
+++ password.js
@@ -7,7 +7,7 @@ const reader = readline.createInterface({
 reader.question("Input Password", function(input){
     tokens = input.split('')
 
-    let character = [1,2,3,4,5,6,7,8,9,10]
+    let character = [1,2,3,4,5,6,7,8,9,10,11]
     let password = []
     
     if(password >= character.length)
